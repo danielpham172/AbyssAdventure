d3a8540bd347120378db45a5feaf24994852eff5
Daniel Pham <daniel.n.pham@gmail.com>
2019-01-06 23:28:16 -0800

    Combined some code for the pickup items

--- core/src/com/abyad/actor/mapobjects/items/AutoItem.java
+++ core/src/com/abyad/actor/mapobjects/items/AutoItem.java
@@ -4,12 +4,28 @@ import java.util.ArrayList;
 
 import com.abyad.actor.entity.PlayerCharacter;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 
 public abstract class AutoItem extends MapItem{
+	
+	private ArrayList<Rectangle> collideBox;
+	private ArrayList<Rectangle> temporaryCollideBox;
+	
+	private static Rectangle baseBox = new Rectangle(0, 0, 5, 5);
 	
 	public AutoItem(TextureRegion tex, float followRadius, boolean following) {
 		super(tex, followRadius);
 		setFollowing(following);
+		collideBox = new ArrayList<Rectangle>();
+		temporaryCollideBox = new ArrayList<Rectangle>();
+		
+		Rectangle box = new Rectangle(baseBox);
+		Rectangle tempBox = new Rectangle(baseBox);
+		
+		collideBox.add(box);
+		temporaryCollideBox.add(tempBox);
+		
+		updateCollideAndInteractBox();
 	}
 	
 	@Override
@@ -28,6 +44,23 @@ public abstract class AutoItem extends MapItem{
 				}
 			}
 		}
+	}
+	
+	@Override
+	public void updateCollideAndInteractBox() {
+		Rectangle box = collideBox.get(0);
+		box.setPosition(getX() - (box.getWidth() / 2), getY() - (box.getHeight() / 2));
+	}
+
+	@Override
+	public ArrayList<Rectangle> getCollideBox() {
+		return collideBox;
+	}
+
+	@Override
+	public ArrayList<Rectangle> getCollideBox(float x, float y) {
+		temporaryCollideBox.get(0).setPosition(x - (temporaryCollideBox.get(0).getWidth() / 2), y - (temporaryCollideBox.get(0).getHeight() / 2));
+		return temporaryCollideBox;
 	}
 
 	public abstract void playerPickup(PlayerCharacter player);
