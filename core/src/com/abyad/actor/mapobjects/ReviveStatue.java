78ea8de0b43b0440fc6a07be02176bb07dd966e9
Daniel Pham <daniel.n.pham@gmail.com>
2019-01-31 00:57:07 -0800

    Goddess statues can also heal players if not carrying a corpse

--- core/src/com/abyad/actor/mapobjects/ReviveStatue.java
+++ core/src/com/abyad/actor/mapobjects/ReviveStatue.java
@@ -30,18 +30,22 @@ public class ReviveStatue extends Actor implements Interactable{
 	private static TextureRegion goddessStatue = AbstractSpriteSheet.spriteSheets.get("STATUE").getSprite("GODDESS_STATUE");
 	private static Rectangle baseHitbox = new Rectangle(0, 0, 22, 22);
 	private static Rectangle baseInteractBox = new Rectangle(0, 0, 32, 32);
-	private static final int GOLD_COST = 100;
+	private static final int HEAL_GOLD_COST = 50;
+	private static final int REVIVE_GOLD_COST = 100;
 	
 	private int timeSinceLastParticle;
 	private boolean active;
 	
 	private static final float FONT_SCALE = 0.25f;
 	private static BitmapFont font = Assets.manager.get(Assets.font);
-	private static GlyphLayout descText = new GlyphLayout();
+	private static GlyphLayout healText = new GlyphLayout();
+	private static GlyphLayout reviveText = new GlyphLayout();
+	private static final float SPACING = 4f;
 	
 	static {
 		font.getData().setScale(FONT_SCALE);
-		descText.setText(font, "REVIVE A PLAYER FOR " + GOLD_COST + " GOLD!");
+		healText.setText(font, "HEAL YOURSELF FOR " + HEAL_GOLD_COST + " GOLD!");
+		reviveText.setText(font, "REVIVE A PLAYER FOR " + REVIVE_GOLD_COST + " GOLD!");
 		font.getData().setScale(1.0f);
 	}
 	
@@ -80,20 +84,28 @@ public class ReviveStatue extends Actor implements Interactable{
 	
 	@Override
 	public void draw(Batch batch, float a) {
-		if (floor.inView()) {
+		if (inView()) {
 			batch.draw(goddessStatue, getX() - getOriginX(), getY() - getOriginY(),
 					getOriginX(), getOriginY(), goddessStatue.getRegionWidth(), goddessStatue.getRegionHeight(), AbstractTile.TILE_SCALE, AbstractTile.TILE_SCALE, getRotation());
 			if (isInteractable) {
 				font.getData().setScale(FONT_SCALE);
-				float fontX = floor.getCenter().x - (descText.width / 2);
-				float fontY = floor.getCenter().y + (AbstractTile.TILE_LENGTH * 1.5f) - (descText.height / 2);
-				font.draw(batch, descText, fontX, fontY);
+				float fontX = floor.getCenter().x - (reviveText.width / 2);
+				float fontY = floor.getCenter().y + (AbstractTile.TILE_LENGTH * 1.2f) - (reviveText.height / 2);
+				font.draw(batch, reviveText, fontX, fontY);
+				fontX = floor.getCenter().x - (healText.width / 2);
+				fontY += SPACING + (healText.height / 2) + (reviveText.height / 2);
+				font.draw(batch, healText, fontX, fontY);
 				font.getData().setScale(1.0f);
 			}
 		}
 		
 		isInteractable = false;
 		//batch.draw(AbstractTile.box, hitbox.getX(), hitbox.getY(), hitbox.getWidth(), hitbox.getHeight());
+	}
+	
+	public boolean inView() {
+		return getStage().getCamera().frustum.boundsInFrustum(getX(), getY() + (AbstractTile.TILE_LENGTH / 2), 0,
+				AbstractTile.TILE_LENGTH / 2, AbstractTile.TILE_LENGTH, 0);
 	}
 	
 	@Override
@@ -105,12 +117,28 @@ public class ReviveStatue extends Actor implements Interactable{
 	public boolean interact(PlayerCharacter source) {
 		if (active) {
 			if (source.getHeldItem() instanceof CorpseItem) {
-				if (source.getGold() >= GOLD_COST) {
+				if (source.getGold() >= REVIVE_GOLD_COST) {
 					CorpseItem corpse = (CorpseItem)source.getHeldItem();
 					corpse.startReviveProcess(source);
 					source.removeHeldItem();
-					source.modifyGold(-GOLD_COST);
+					source.modifyGold(-REVIVE_GOLD_COST);
 					getStage().addActor(corpse);
+					active = false;
+					interactables.remove(this);
+					return true;
+				}
+				else {
+					BattleText notEnoughGoldText = new BattleText("Not enough gold!", getX(), getY(), new Vector2(0, 1.0f), 1.0f, 25);
+					notEnoughGoldText.setScale(0.2f);
+					getStage().addActor(notEnoughGoldText);
+					return true;
+				}
+			}
+			else {
+				if (source.getGold() >= HEAL_GOLD_COST) {
+					source.modifyHP(source.getMaxHP());
+					source.addMana(source.getMaxMana());
+					source.modifyGold(-HEAL_GOLD_COST);
 					active = false;
 					interactables.remove(this);
 					return true;
